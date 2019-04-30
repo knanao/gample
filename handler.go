1dd7a9f81a7d9aa29d3bb281ee826af82f6966da
knanao <nao.7ken@gmail.com>
2019-05-01 00:10:26 +0900

    create TodoCreate

--- handler.go
+++ handler.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 
@@ -30,6 +32,31 @@ func TodoShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		panic(err)
+	}
+}
+
+func TodoCreate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var todo Todo
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	if err := json.Unmarshal(body, &todo); err != nil {
+		w.WriteHeader(500)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	t := RepoCreateTodo(todo)
+	location := fmt.Sprintf("http://%s/%d", r.Host, t.ID)
+	w.Header().Set("Location", location)
+	w.WriteHeader(http.StatusCreated)
 	if err := json.NewEncoder(w).Encode(t); err != nil {
 		panic(err)
 	}
--- main.go
+++ main.go
@@ -12,5 +12,7 @@ func main() {
 	router.GET("/", utils.Logging(HomeHandle, "home"))
 	router.GET("/todos", utils.CommonHeaders(TodoHome, "todo-home"))
 	router.GET("/todos/:todoId", utils.IdShouldBeInt(TodoShow, "todo-show"))
+	//curl -X POST -H "Content-Type: application/json" -d '{"Name":"hogehoge"}' localhost:8080/todos
+	router.POST("/todos", utils.CommonHeaders(TodoCreate, "todo-create"))
 	http.ListenAndServe(":8080", router)
 }
